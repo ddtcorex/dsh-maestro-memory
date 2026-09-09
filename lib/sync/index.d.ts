83e2b46e64820b27bb6dab1609c19a6afee4815b
Toan Do <kaido4492@gmail.com>
2026-09-09 07:23:35 +0700

    fix: stop tracking lib/ build output

--- lib/sync/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './layout.ts';
-export * from './merge.ts';
-export * from './git.ts';
-export * from './config.ts';
-export * from './service.ts';
-//# sourceMappingURL=index.d.ts.map
